d6bb1b03b81414d9a44eb81ac96fa424a08c8543
Jawshouamoua <kenjos03@ccm-2610-e1.evergreen.edu>
2013-10-03 10:51:49 -0700

    homework 1 solutions
    these are BANZAI!

---
+++ homework1.py
@@ -0,0 +1,52 @@
+# Name: Joshua Kendall
+# Evergreen Login: kenjos03
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+a = 1
+b = -5.86
+c = 8.5408
+
+x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)  
+x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+
+print "x =", x1,",", x2 
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+import hw1_test
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
