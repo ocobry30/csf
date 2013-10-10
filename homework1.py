23fe774919a5349edac807ae3dbf7e4ab9b86b5b
rawjor18 <rawjor18@ccm-2610-f2.evergreen.edu>
2013-10-10 11:56:15 -0700

    hw1_graded

--- homework1.py
+++ homework1.py
@@ -4,6 +4,7 @@
 # Programming as a Way of Life
 # Homework 1
 
+
 # You may do your work by editing this file, or by typing code at the
 # command line and copying it into the appropriate part of this file when
 # you are done.  When you are done, running this file should compute and
